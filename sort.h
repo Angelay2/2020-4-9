c5ec28849762aa738392a3699d3ddc3c84774c2a
Angelay2 <y2zaijian@126.com>
2020-04-09 22:22:11 +0800

    排序头文件

---
+++ sort.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void swap(int a, int b);
+void insertSort(int* array, int n);
+void shellSort(int* array, int n);
+void selectSort(int* array, int n);
+void selectSort2(int* array, int n);
+
+void shiftDown(int* array, int n, int parent);
+void heapSort(int* array, int n);
