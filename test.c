bea470f36ef118275b122cd5f7aa2d6a7bda0449
Angelay2 <y2zaijian@126.com>
2020-04-13 22:11:02 +0800

    排序测试

--- test.c
+++ test.c
@@ -8,6 +8,33 @@ void printArray(int* a, int n){
 	printf("\n");
 }
 
-void test(){
+void test1(){
+	
+	int array[] = { 10, -1, 2, 9, 3, 5, 7, 24, -5, 100 };
+	int num = sizeof(array) / sizeof(array[0]);
+	printArray(array, num);
+	insertSort(array, num);
+	printArray(array, num);
+}
+int test2(){
+	
 	int* array = (int*)malloc(sizeof(int));
+}
+
+int test3(){
+	int array[] = { 10, -1, 2, 9, 3, 5, 7, 24, -5, 100 };
+	int num = sizeof(array) / sizeof(array[0]);
+	int begin = array[0];
+	int end = array[num - 1];
+	printArray(array, num);
+	quickSort(array, begin, end);
+	printArray(array, num);
+}
+int main(){
+
+	// test1();
+	test3();
+	system("pause");
+	return 0;
+
 }
